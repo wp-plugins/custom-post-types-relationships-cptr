eab8a5558c9a9ebed280f350f8e34eeaffe6f6ea
anastis <anastis@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-09-18 00:59:54 +0000

    Made strings in panel.php translatable.
    git-svn-id: https://plugins.svn.wordpress.org/custom-post-types-relationships-cptr/trunk@774331 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- panel.php
+++ panel.php
@@ -1,3 +1,4 @@
+
 <?php
 // Build the admin panel
 
@@ -53,11 +54,9 @@ function ci_cptr_plugin_options() {
 	}
 	?>
 	<div class="wrap">
-		<h2>CSSIgniter Custom Post Types Relationships v<?php echo CPTR_VERSION; ?> - Settings</h2>
-		<p>In this page you can define general options for the Custom Post Types Relationships plugin. All options here can be overridden 
-		manually by passing the appropriate parameters to the shortcode or the theme function. If you find yourself making changes here 
-		that don't have any effect, it's because your WordPress Theme has hardcoded options for you, so check with the theme's developer.</p>
-		<p>For complete usage instructions, plase visit the <a href="http://www.cssigniter.com/ignite/custom-post-types-relationships/">plugin's homepage</a>.</p>
+		<h2><?php echo sprintf(__('CSSIgniter Custom Post Types Relationships v%s - Settings', 'cptr'), CPTR_VERSION); ?></h2>
+		<p><?php __("In this page you can define general options for the Custom Post Types Relationships plugin. All options here can be overridden manually by passing the appropriate parameters to the shortcode or the theme function. If you find yourself making changes here that don't have any effect, it's because your WordPress Theme has hardcoded options for you, so check with the theme's developer.", 'cptr'); ?></p>
+		<p><?php echo sprintf(__('For complete usage instructions, please visit the <a href="%s">plugin\'s homepage</a>.', 'cptr'), 'http://www.cssigniter.com/ignite/custom-post-types-relationships/'); ?></p>
 		<form method="post" action="options.php">
 			<?php settings_fields('ci_cptr_plugin_settings'); ?>
 	
@@ -68,41 +67,41 @@ function ci_cptr_plugin_options() {
 
 			<table class="form-table">
 				<tr valign="top">
-					<th scope="row">Max number of displayed related posts:</th>
+					<th scope="row"><?php _e('Max number of displayed related posts:', 'cptr'); ?></th>
 					<td>
 						<input name="<?php echo CI_CPTR_PLUGIN_OPTIONS; ?>[limit]" type="text" value="<?php echo $options['limit']; ?>" />
-						<p>Set to 0 for no limit.</p>
+						<p><?php _e('Set to 0 for no limit.', 'cptr'); ?></p>
 					</td>
 				</tr>
 
 				<tr valign="top">
-					<th scope="row">Show the excerpt for each post?</th>
+					<th scope="row"><?php _e('Show the excerpt for each post?', 'cptr'); ?></th>
 					<td>
 						<input name="<?php echo CI_CPTR_PLUGIN_OPTIONS; ?>[excerpt]" type="checkbox" value="1" <?php checked($options['excerpt'], 1); ?> />
 					</td>
 				</tr>
 
 				<tr valign="top">
-					<th scope="row">How many words the excerpt should be?</th>
+					<th scope="row"><?php _e('How many words the excerpt should be?', 'cptr'); ?></th>
 					<td>
 						<input name="<?php echo CI_CPTR_PLUGIN_OPTIONS; ?>[words]" type="text" value="<?php echo $options['words']; ?>" />
 					</td>
 				</tr>
 
 				<tr valign="top">
-					<th scope="row">Display the thumbnail?</th>
+					<th scope="row"><?php _e('Display the thumbnail?', 'cptr'); ?></th>
 					<td>
 						<input name="<?php echo CI_CPTR_PLUGIN_OPTIONS; ?>[thumb]" type="checkbox" value="1" <?php checked($options['thumb'], 1); ?> />
-						<p>The thumbnail will be displayed after the title and before the excerpt.</p>
+						<p><?php _e('The thumbnail will be displayed after the title and before the excerpt.', 'cptr'); ?></p>
 					</td>
 				</tr>
 
 				<tr valign="top">
-					<th scope="row">Thumbnail size</th>
+					<th scope="row"><?php _e('Thumbnail size', 'cptr'); ?></th>
 					<td>
-						<label>Width</label>
+						<label><?php _e('Width', 'cptr'); ?></label>
 						<input name="<?php echo CI_CPTR_PLUGIN_OPTIONS; ?>[width]" type="text" value="<?php echo $options['width']; ?>" />
-						<br /><label>Height</label>
+						<br /><label><?php _e('Height', 'cptr'); ?></label>
 						<input name="<?php echo CI_CPTR_PLUGIN_OPTIONS; ?>[height]" type="text" value="<?php echo $options['height']; ?>" />
 					</td>
 				</tr>
